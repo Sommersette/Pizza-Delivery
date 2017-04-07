1277d9b53f87bf2058aab38d8b3f27ba9e6c54bc
Trevor Fast <trevorrfast@gmail.com>
2017-04-07 14:19:52 -0700

    add full protoypes and corrected objects. Fix css styles. Change location of add  button on order form.

--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,39 @@
 
 //foreach branch id dining take away or delivery options
 
-function Order {
+function Order(address, pizza) {
   this.address[];
   this.pizza[];
 }
 
-function Address {
+function Address(name, street, state, zip) {
   this.name = name;
   this.street = street;
   this.city = state;
   this.zip = zip;
 }
 
-function Pizza {
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 
 }
 
-Order.prototype.finalOrder (){
-  return this.address + " " + this.pizza
+Order.prototype.finalOrder = function(){
+  return this.address + ", " + this.pizza;
 }
 
-Address.prototype {
-  return this.name + " " + this.street + " " + this.city, + " " + this.zip
+Address.prototype.fullAddress = function() {
+  return this.name + ", " + this.street + ", " + this.city, + ", " + this.zip;
+}
+
+Pizza.prototype.pizzaOrder = function() {
+  return this.size + ", " + this.toppings;
 }
 // FRONT END //
+
+$(document).ready(function() {
+  $("").click(function() {
+
+  });
+});
