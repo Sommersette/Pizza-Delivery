b8f7fc155dd61e2bb87e53309edef4957c6daba7
Trevor Fast <trevorrfast@gmail.com>
2017-04-11 23:49:39 -0700

    add functionality to the javascript so it will toggle the topping images on to the pie and increment the price per topping. It will also increment the price in accordance with the size radio button checked. The HTML was updated with supporting attributes, classes, & ids so as to be properly manipulated by the javascript. Add a new  CSS rule so the HTML could be influenced to hide and or show the appropriate element.

--- js/scripts.js
+++ js/scripts.js
@@ -1,165 +1,179 @@
 // BACKEND //
+  // function grandTotal(order, address) {
+  //   this.order = [];
+  //   this.address = [];
+  // }
 
-function grandTotal(order, address) {
-  this.order = [];
-  this.address = [];
-}
-
-function Order(pizza, toppings, delivery) {
-    this.size = size;
-    this.toppings = toppings;
-    this.delivery = delivery;
-  }
-
+  function Order(toppings, size, delivery) {
+      this.toppings = toppings;
+      this.size = size;
+      this.delivery = delivery;
+    }
   // function Address(street, city, state, zip) {
   //   this.street = street;
   //   this.city = city;
   //   this.state = state;
   //   this.zip = zip;
   // }
-
-Order.prototype.pizzaOrder = function() {
-  return this.size + ", " + this.toppings + "," + this.delivery;
-}
-
-// Address.prototype.fulladdress = function() {
-//   return this.street + ", " + this.city + ", " + this.state + "," + this.zip;
-// }
-
-//push to this object with conditional for each topping and do one for size as well based on the length of each array that it is for.
-function grandTotal(size, toppings, delivery) {
-  if (size === "small") {
-    var sizePrice = 10;
-  } else if (size === "medium"){
-    var sizePrice = 12;
-  } else if (size === "large") {
-    sizePrice = 14;
-  } else if (toppings.length === 5) {
-    toppingPrice = 6.25;
-  } else if (toppings.length === 4) {
-    toppingPrice = 5.00;
-  } else if (toppings.length === 3) {
-      toppingPrice = 3.75;
-  } else if (toppings.length === 2) {
-    toppingPrice = 2.50;
-  } else if (toppings.lenght === 1){
-    toppingsPrice = 1.25;
-  } else if (delivery === true) {
-    deliveryPrice = 2.00;
-  }
-  subtotal = (sizePrice+toppingPrice+deliveryPrice);
-  return subtotal;
-}
-
-  function returnGrandTotal() {
-    grandTotal.push(parseFloat(subtotal));
-  }
-
-    function grandOrder() {
-      var toPrice = 0;
-      grandTotal.forEach(function(price){
-        toPrice += price;
-      })
-        return toPrice;
-    }
-
-//////////////////////// FRONT END ///////////////////////////////
-
-
-//branching
-$(document).ready(function() {
-
-  $("#dining").click(function(event) {
-  event.preventDefault();
-  var delivery = []
-    var inOrOut = $("input:radio[name=dining]:checked").val();
-    var delivery = $(this).val();
-    if (inOrOut === "delivery") {
-      delivery.push(delivery);
-      $("#address").show();
-      $("#dining").hide();
+  Order.prototype.pizzaPrice = function(toppings, size, delivery) {
+    var priceSize = 0;
+    if (this.size === "small") {
+      priceSize += 1.00
+    } else if (this.size === "medium"){
+      priceSize += 120.00
     } else {
-      $("#address, #dining").hide();
-      $("#orderscreen, footer").show();
+      priceSize += 4000.00
+    }
+    var priceToppings = 0;
+    if (this.toppings.length === 1) {
+      priceToppings += 1.25
+    } else if (this.toppings.length === 2) {
+      priceToppings += 2.50
+    } else if (this.toppings.length === 3) {
+      priceToppings += 3.75
+    } else if (this.toppings.length === 4) {
+      priceToppings += 5.00
+    } else {
+      priceToppings += 6.25
+    }
+
+    var priceDelivery = 0;
+
+    if (this.delivery) {
+      priceDelivery += 3;
+    } else {
+      priceDelivery +=0;
+    }
+
+
+    return priceSize + priceToppings + priceDelivery;
+  }
+
+  // Address.prototype.deliveryPrice = function() {
+  //   if (delivery === true) {
+  //     this.order += 2.50
+  //   } else {
+  //    this.order += 0.00
+  //   }
+  // }
+////////////////////////// FRONT END /////////////////////////////
+
+  $(document).ready(function() {
+
+    $(".dining").click(function(event) {
+    event.preventDefault();
+    var delivery = false;
+    var inOrOut = $("input:radio[name=dining]:checked").val();
+      // var delivery = $(this).val();
+      if (inOrOut === "delivery") {
+        delivery = true;
+
+        // delivery.push(delivery);
+        $("#address").show();
+        $("#dining").hide();
+      } else if (inOrOut === "takeaway") {
+        delivery= false;
+        $("#address, #dining").hide();
+        $("#orderscreen, footer").show();
+      }
+      console.log(delivery);
+    });
+
+    $("#address").submit(function(event){
+      event.preventDefault();
+      var addressForm = $("#fullName").val();
+      $("#address").hide();
+      $("#orderscreen").show();
+      console.log(addressForm);
+    });
+
+    $("#add").click(function(event){
+      var size = $("input:radio[name=size]:checked").val();
+      var toppings=[];
+      $("input:checkbox[name=toppings]:checked").each(function() {
+        var topping = $(this).val();
+        toppings.push(topping);
+      })
+      var newOrder= new Order(toppings,size,delivery);
+      toppings.forEach(function(topping) {
+      $("#pizzadescription").append("<li>" + topping + "</li>");
+      });
+      $("#subtotal").append(newOrder.pizzaPrice());
+
+      $("#placeOrder").click(function() {
+        $("#confirmation").show();
+        $("#totalprice").text("why");
+        $("#totalprice").text("thank you for your order. The total price is" + newOrder.pizzaPrice());
+      })
+    });
+
+
+    $("#size").change(function(event){
+    event.preventDefault();
+    var Size = []
+    var size =  $("input:radio[name=size]:checked").val();
+    if (size === "large") {
+      $("#small").hide();
+      $("#medium").hide();
+      $("#large").show();
+      $("#sizes").hide();
+      $("#crust").show();
+      $("#toppings").show();
+      //price: append DOM #pizzadescription <ul><li></li></ul>
+    } else if (size === "medium") {
+      $("#small").hide();
+      $("#medium").show();
+      $("#large").hide();
+      $("#sizes").hide();
+      $("#crust").show();
+      $("#toppings").show();
+
+      //price: append DOM #pizzadescription <ul><li></li></ul>
+    } else {
+      $("#small").show();
+      $("#medium").hide();
+      $("#large").hide();
+      $("#sizes").hide();
+      $("#crust").show();
+      $("#toppings").show()
+
+      //price: append DOM #pizzadescription <ul><li></li></ul>
     }
   });
 
-  $("#address").submit(function(event){
-    event.preventDefault();
-    var addressForm = $("#address")
-    $("#address").hide();
-    $("#orderscreen").show();
-  });
+    $("#cheeseCheck").change(function(){
+      $("#cheese").toggle();
+    });
 
+    $("#pepperoniCheck").change(function(){
+      $("#pepperoni").toggle();
+    });
 
-  $("#size").change(function(event){
-  event.preventDefault();
-  var Size = []
-  var size =  $("input:radio[name=size]:checked").val();
-  if (size === "large") {
-    $("#small").hide();
-    $("#medium").hide();
-    $("#large").show();
-    $("#sizes").hide();
-    $("#crust").show();
-    $("#toppings").show();
-    //price: append DOM #pizzadescription <ul><li></li></ul>
-  } else if (size === "medium") {
-    $("#small").hide();
-    $("#medium").show();
-    $("#large").hide();
-    $("#sizes").hide();
-    $("#crust").show();
-    $("#toppings").show();
+    $("#artichokeCheck").change(function(){
+      $("#artichokes").toggle();
+    });
 
-    //price: append DOM #pizzadescription <ul><li></li></ul>
-  } else {
-    $("#small").show();
-    $("#medium").hide();
-    $("#large").hide();
-    $("#sizes").hide();
-    $("#crust").show();
-    $("#toppings").show()
+    $("#anchovyCheck").change(function(){
+      $("#anchovy").toggle();
+    });
 
-    //price: append DOM #pizzadescription <ul><li></li></ul>
-  }
+    $("#mushroomCheck").change(function(){
+      $("#mushrooms").toggle();
+    });
+
+  // need to write code for add button to subtotal the cost and order.
 });
 
-  $("#cheeseCheck").change(function(){
-    $("#cheese").toggle();
-  });
-
-  $("#pepperoniCheck").change(function(){
-    $("#pepperoni").toggle();
-  });
-
-  $("#artichokeCheck").change(function(){
-    $("#artichokes").toggle();
-  });
-
-  $("#anchovyCheck").change(function(){
-    $("#anchovy").toggle();
-  });
-
-  $("#mushroomCheck").change(function(){
-    $("#mushrooms").toggle();
-  });
-
-// need to write code for add button to subtotal the cost and order.
-
-  // function resetForm() {
-  //   $("input#cheese").val("");
-  //   $("input#pepperoni").val("");
-  //   $("input#anchovy").val("");
-  //   $("input#artichokes").val("");
-  //   $("input#size").val("");
-  //   $(input"#mushrooms").val("");
-  // }
-  // resetForm
-  //(so  can  add extra  pizzas.)
-  // add button clears form/subtotals price and moves item to order description column
-  // address added to address box in right column
-  // place order shows grand total and order screen.
-
-
-});
+    // function resetForm() {
+    //   $("input#cheese").val("");
+    //   $("input#pepperoni").val("");
+    //   $("input#anchovy").val("");
+    //   $("input#artichokes").val("");
+    //   $("input#size").val("");
+    //   $(input"#mushrooms").val("");
+    // }
+    // resetForm
+    //(so  can  add extra  pizzas.)
+    // add button clears form/subtotals price and moves item to order description column
+    // address added to address box in right column
+    // place order shows grand total and order screen.
